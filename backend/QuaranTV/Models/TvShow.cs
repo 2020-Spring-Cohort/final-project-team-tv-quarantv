255f0f211dd07fbd0d297d13d00e25229ac8dd8b
Kashmellow <ejodlowsky@pm.me>
2020-03-31 11:45:11 -0400

    added TvShow model with Id and Name props

---
+++ backend/QuaranTV/Models/TvShow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuaranTV.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
