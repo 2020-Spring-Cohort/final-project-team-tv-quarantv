9ab70ab27d19756ce39fde16cfd01bbc978743b2
Eric Drexler <ericdrxlr@gmail.com>
2020-03-31 13:31:20 -0400

    added repositories

---
+++ backend/QuaranTV/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuaranTV.Controllers.Repositories
+{
+    public class IRepository
+    {
+    }
+}
