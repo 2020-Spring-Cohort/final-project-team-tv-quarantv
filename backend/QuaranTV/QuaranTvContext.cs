9e5e8458b0503ae41bd8b88f07bddd8666cd0867
Eric Drexler <ericdrxlr@gmail.com>
2020-03-31 13:48:17 -0400

    added context class and added our model dbsets

---
+++ backend/QuaranTV/QuaranTvContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuaranTV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuaranTV
+{
+    public class QuaranTvContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<TvShow> TvShows { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+      
+      
+    }
+}
