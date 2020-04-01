a015f1be8a368316fea291194fec24d4a94a447b
ghassan <gh09an@gmail.com>
2020-04-01 14:13:12 -0400

    many to many usertvshow

--- backend/QuaranTV/Models/UserTvShow.cs
+++ backend/QuaranTV/Models/UserTvShow.cs
@@ -8,10 +8,10 @@ namespace QuaranTV.Models
 {
     public class UserTvShow
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
-        public string Name { get; set; }
-
+        public virtual User User { get; set; }
         public int TvShowId { get; set; }
-        public string Title { get; set; }
+        public virtual TvShow TvShow { get; set; }
     }
 }
