f60eeb020174fc26d267f1bb1dd3b5b1180beaa9
ghassan <gh09an@gmail.com>
2020-04-02 13:18:35 -0400

    edited users.js components

--- frontend/src/js/components/Users.js
+++ frontend/src/js/components/Users.js
@@ -6,9 +6,10 @@ export default function Users(users){
                 return `
                 <div class="users__specific_user"
                     <h4 class="user__name_btn">${user.name}</h4>
+                    <input class="user__id" type="hidden" value="${user.id}">
                 </div>
                `
-        })}
+        }).join("")}
         </div>
     `
 }
