978aadc84fb4c7a44756da7311b2db3a32796f73
ghassan <gh09an@gmail.com>
2020-04-02 11:30:55 -0400

    added footer and navbar component

---
+++ frontend/src/js/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+`<nav class='navbar_list'>
+         <ul>
+         <li class='nav__home'> <h3  class="btn btn-ghost ">Home</h3></li>
+         <li class='nav__users'> <h3  class="btn btn-ghost ">Users</h3></li>
+         <li class='nav__tvshows'> <h3  class="btn btn-ghost ">Tv Shows</h3></li>
+         <li class='nav__aboutus'> <h3  class="btn btn-ghost ">About Us</h3></li>
+         </ul>
+    </nav>` 
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,2 +1,26 @@
 //import here
 import apiActions from "./api/apiActions";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+export default pageBuild
+
+function pageBuild(){
+    header();
+    navbar();
+    footer();
+}
+
+function header() {
+    const header = document.querySelector('.header');
+    header.innerHTML = Header();
+}
+function navbar() {
+    const navbar = document.querySelector('.navbar');
+    navbar.innerHTML = Navbar();
+}
+function footer() {
+    const footer = document.querySelector('.footer');
+    footer.innerHTML = Footer();
+}
