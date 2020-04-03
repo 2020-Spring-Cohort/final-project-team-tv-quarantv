6765cc25d849c9a2d0327d311492dbe0d718190f
Dan Rasmussen <drasmussen49@gmail.com>
2020-04-03 11:55:29 -0400

    Removed old migrations, added new migration for new/modified models

--- backend/QuaranTV/Migrations/20200403154426_FirstNewModelsMigration.cs
+++ backend/QuaranTV/Migrations/20200403154426_FirstNewModelsMigration.cs
@@ -2,7 +2,7 @@
 
 namespace QuaranTV.Migrations
 {
-    public partial class FirstMigration : Migration
+    public partial class FirstNewModelsMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -14,7 +14,7 @@ namespace QuaranTV.Migrations
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Title = table.Column<string>(nullable: true),
                     Image = table.Column<string>(nullable: true),
-                    Season = table.Column<int>(nullable: false),
+                    Season = table.Column<string>(nullable: true),
                     ViewerDiscretion = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
@@ -37,53 +37,28 @@ namespace QuaranTV.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "Comments",
+                name: "Watchlists",
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Body = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Review = table.Column<string>(nullable: true),
                     Rating = table.Column<string>(nullable: true),
                     UserId = table.Column<int>(nullable: false),
                     TvShowId = table.Column<int>(nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.PrimaryKey("PK_Watchlists", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Comments_TvShows_TvShowId",
+                        name: "FK_Watchlists_TvShows_TvShowId",
                         column: x => x.TvShowId,
                         principalTable: "TvShows",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_Comments_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "UserTvShows",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    UserId = table.Column<int>(nullable: false),
-                    TvShowId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_UserTvShows", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_UserTvShows_TvShows_TvShowId",
-                        column: x => x.TvShowId,
-                        principalTable: "TvShows",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_UserTvShows_Users_UserId",
+                        name: "FK_Watchlists_Users_UserId",
                         column: x => x.UserId,
                         principalTable: "Users",
                         principalColumn: "Id",
@@ -91,33 +66,20 @@ namespace QuaranTV.Migrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Comments_TvShowId",
-                table: "Comments",
+                name: "IX_Watchlists_TvShowId",
+                table: "Watchlists",
                 column: "TvShowId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Comments_UserId",
-                table: "Comments",
-                column: "UserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserTvShows_TvShowId",
-                table: "UserTvShows",
-                column: "TvShowId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_UserTvShows_UserId",
-                table: "UserTvShows",
+                name: "IX_Watchlists_UserId",
+                table: "Watchlists",
                 column: "UserId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Comments");
-
-            migrationBuilder.DropTable(
-                name: "UserTvShows");
+                name: "Watchlists");
 
             migrationBuilder.DropTable(
                 name: "TvShows");
--- backend/QuaranTV/Migrations/QuaranTvContextModelSnapshot.cs
+++ backend/QuaranTV/Migrations/QuaranTvContextModelSnapshot.cs
@@ -18,84 +18,6 @@ namespace QuaranTV.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("QuaranTV.Models.Comment", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("Body")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Rating")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("TvShowId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("UserId")
-                        .HasColumnType("int");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("TvShowId");
-
-                    b.HasIndex("UserId");
-
-                    b.ToTable("Comments");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 12,
-                            UserId = 2
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 15,
-                            UserId = 7
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 13,
-                            UserId = 3
-                        },
-                        new
-                        {
-                            Id = 4,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 3,
-                            UserId = 4
-                        },
-                        new
-                        {
-                            Id = 5,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 8,
-                            UserId = 4
-                        },
-                        new
-                        {
-                            Id = 6,
-                            Body = "placeholder text",
-                            Rating = "placeholder rating",
-                            TvShowId = 15,
-                            UserId = 2
-                        });
-                });
-
             modelBuilder.Entity("QuaranTV.Models.TvShow", b =>
                 {
                     b.Property<int>("Id")
@@ -106,8 +28,8 @@ namespace QuaranTV.Migrations
                     b.Property<string>("Image")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<int>("Season")
-                        .HasColumnType("int");
+                    b.Property<string>("Season")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Title")
                         .HasColumnType("nvarchar(max)");
@@ -118,128 +40,6 @@ namespace QuaranTV.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("TvShows");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Image = "friends.jpg",
-                            Season = 10,
-                            Title = "Friends",
-                            ViewerDiscretion = "TV-14"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Image = "gameofthrones.jpg",
-                            Season = 8,
-                            Title = "Game of Thrones",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Image = "maisel.jpg",
-                            Season = 4,
-                            Title = "The Marvelous Mrs. Maisel",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 4,
-                            Image = "ozark.jpg",
-                            Season = 3,
-                            Title = "Ozark",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 5,
-                            Image = "freshprince.jpg",
-                            Season = 6,
-                            Title = "The Fresh Prince",
-                            ViewerDiscretion = "TV-PG"
-                        },
-                        new
-                        {
-                            Id = 6,
-                            Image = "goldengirls.jpg",
-                            Season = 7,
-                            Title = "The Golden Girls",
-                            ViewerDiscretion = "TV-PG"
-                        },
-                        new
-                        {
-                            Id = 7,
-                            Image = "tigerking.jpg",
-                            Season = 1,
-                            Title = "Tiger King",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 8,
-                            Image = "strangerthings.jpg",
-                            Season = 4,
-                            Title = "Stranger Things",
-                            ViewerDiscretion = "TV-14"
-                        },
-                        new
-                        {
-                            Id = 9,
-                            Image = "dearwhitepeople.jpg",
-                            Season = 4,
-                            Title = "Dear White People",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 10,
-                            Image = "mandalorian.jpg",
-                            Season = 1,
-                            Title = "The Mandalorian",
-                            ViewerDiscretion = "TV-PG"
-                        },
-                        new
-                        {
-                            Id = 11,
-                            Image = "atlanta.jpg",
-                            Season = 3,
-                            Title = "Atlanta",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 12,
-                            Image = "alteredcarbon.jpg",
-                            Season = 2,
-                            Title = "Altered Carbon",
-                            ViewerDiscretion = "TV-MA"
-                        },
-                        new
-                        {
-                            Id = 13,
-                            Image = "newgirl.jpg",
-                            Season = 7,
-                            Title = "New Girl",
-                            ViewerDiscretion = "TV-14"
-                        },
-                        new
-                        {
-                            Id = 14,
-                            Image = "blindspot.jpg",
-                            Season = 5,
-                            Title = "Blind Spot",
-                            ViewerDiscretion = "TV-14"
-                        },
-                        new
-                        {
-                            Id = 15,
-                            Image = "theamazingrace.jpg",
-                            Season = 32,
-                            Title = "The Amazing Race",
-                            ViewerDiscretion = "TV-PG"
-                        });
                 });
 
             modelBuilder.Entity("QuaranTV.Models.User", b =>
@@ -258,64 +58,23 @@ namespace QuaranTV.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Users");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Image = "dan.jpg",
-                            Name = "Dan"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Image = "nabil.jpg",
-                            Name = "Nabil"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Image = "tatyana.jpg",
-                            Name = "Tatyana"
-                        },
-                        new
-                        {
-                            Id = 4,
-                            Image = "erin.jpg",
-                            Name = "Erin"
-                        },
-                        new
-                        {
-                            Id = 5,
-                            Image = "eric.jpg",
-                            Name = "Eric"
-                        },
-                        new
-                        {
-                            Id = 6,
-                            Image = "ghassan.jpg",
-                            Name = "Ghassan"
-                        },
-                        new
-                        {
-                            Id = 7,
-                            Image = "karlyn.jpg",
-                            Name = "Karlyn"
-                        },
-                        new
-                        {
-                            Id = 8,
-                            Image = "carlton.jpg",
-                            Name = "Carlton"
-                        });
                 });
 
-            modelBuilder.Entity("QuaranTV.Models.UserTvShow", b =>
+            modelBuilder.Entity("QuaranTV.Models.Watchlist", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Rating")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Review")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Status")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<int>("TvShowId")
                         .HasColumnType("int");
@@ -329,34 +88,19 @@ namespace QuaranTV.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("UserTvShows");
+                    b.ToTable("Watchlists");
                 });
 
-            modelBuilder.Entity("QuaranTV.Models.Comment", b =>
+            modelBuilder.Entity("QuaranTV.Models.Watchlist", b =>
                 {
                     b.HasOne("QuaranTV.Models.TvShow", "TvShow")
-                        .WithMany("Comments")
+                        .WithMany("Watchlists")
                         .HasForeignKey("TvShowId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
                     b.HasOne("QuaranTV.Models.User", "User")
-                        .WithMany("Comments")
-                        .HasForeignKey("UserId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-                });
-
-            modelBuilder.Entity("QuaranTV.Models.UserTvShow", b =>
-                {
-                    b.HasOne("QuaranTV.Models.TvShow", "TvShow")
-                        .WithMany("UserTvShows")
-                        .HasForeignKey("TvShowId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("QuaranTV.Models.User", "User")
-                        .WithMany("UserTvShows")
+                        .WithMany("Watchlists")
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
