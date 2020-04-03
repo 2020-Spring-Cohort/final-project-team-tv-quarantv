14f141f8bd5e03a1a1793d704a2d5bedad8b0501
Dan Rasmussen <drasmussen49@gmail.com>
2020-04-03 12:02:33 -0400

    Added seed data migration to new tables

--- backend/QuaranTV/Migrations/20200403155922_NewModelsAddSeedData.Designer.cs
+++ backend/QuaranTV/Migrations/20200403155922_NewModelsAddSeedData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuaranTV;
 
 namespace QuaranTV.Migrations
 {
     [DbContext(typeof(QuaranTvContext))]
-    partial class QuaranTvContextModelSnapshot : ModelSnapshot
+    [Migration("20200403155922_NewModelsAddSeedData")]
+    partial class NewModelsAddSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
