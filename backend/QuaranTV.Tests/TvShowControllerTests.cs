ca20d47aab2d6228fc0bc4b8223467e3802d7256
Eric Drexler <ericdrxlr@gmail.com>
2020-04-06 13:34:13 -0400

    working on tests

---
+++ backend/QuaranTV.Tests/TvShowControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using QuaranTV.Controllers;
+using QuaranTV.Models;
+using QuaranTV.Repositories;
+using System.Collections.Generic;
+using System;
+using Xunit;
+using NSubstitute;
+using System.Linq;
+
+namespace QuaranTV.Tests
+{
+    public class TvShowControllerTests
+    {
+        TvShowController testController;
+        IRepository<TvShow> tvShowMockRepo;
+
+        public TvShowControllerTests()
+        {
+            tvShowMockRepo = Substitute.For<IRepository><TvShow>>();
+            testController = new TvShowController(tvShowMockRepo);
+        }
+    }
+}
