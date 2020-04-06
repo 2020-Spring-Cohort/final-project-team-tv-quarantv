aa8f485607575f7250bbe2046cc409f115e2a7b0
Tatyana Bailey <btatyana45@gmail.com>
2020-04-06 14:44:18 -0400

    added profile links to imgs

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -1,11 +1,9 @@
 export default function AboutUs() {
     return `
     <p id="about-us">This is the About Us page</p>
-    <img class="users__user_image" src="./img/users/dan.jpg">
-    <img class="users__user_image" src="./img/users/eric.jpg">
-    <img class="users__user_image" src="./img/users/ghassan.jpg">
-    <img class="users__user_image" src="./img/users/tatyana.jpg" href="" target="_blank"></img>
-
-    
+    <a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"></a>
+    <a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img src="./img/users/ghassan.jpg" class="users__user_image"></a>
+    <a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img src="./img/users/dan.jpg" class="users__user_image"></a>
+    <a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
 `
 }
