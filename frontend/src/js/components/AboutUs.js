8957be944317d932aeb7baadd989c7df96c0d437
Tatyana Bailey <btatyana45@gmail.com>
2020-04-06 14:25:36 -0400

    connected images to aboutUs component

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -1,5 +1,11 @@
 export default function AboutUs() {
     return `
     <p id="about-us">This is the About Us page</p>
+    <img class="users__user_image" src="./img/users/dan.jpg">
+    <img class="users__user_image" src="./img/users/eric.jpg">
+    <img class="users__user_image" src="./img/users/ghassan.jpg">
+    <img class="users__user_image" src="./img/users/tatyana.jpg" href="" target="_blank"></img>
+
+    
 `
 }
