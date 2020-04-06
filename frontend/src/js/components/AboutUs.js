a2bcbe14a52ba70f542f9517e6fa49a47e7cdc45
Tatyana Bailey <btatyana45@gmail.com>
2020-04-06 15:12:53 -0400

    working on user image layout in AboutUs

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -1,9 +1,11 @@
 export default function AboutUs() {
     return `
     <p id="about-us">This is the About Us page</p>
+    <div id="allpics">
     <a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"></a>
     <a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img src="./img/users/ghassan.jpg" class="users__user_image"></a>
     <a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img src="./img/users/dan.jpg" class="users__user_image"></a>
     <a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
-`
+    </div>
+    `
 }
