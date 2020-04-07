330e0e5466d5ad2518b5dce7ac9d0bc7d99bb8c0
Eric Drexler <ericdrxlr@gmail.com>
2020-04-07 14:09:47 -0400

    fixed more names

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -3,13 +3,19 @@ export default function AboutUs() {
     <p id="about-us">This is the About Us page</p>
     <div id="allpics">
     <div><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"></a>
-    <p>Tatyana</p></div>
+    <p>Tatyana Bailey</p></div>
     <div><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img src="./img/users/ghassan.jpg" class="users__user_image"></a>
-    <p>Ghassan</p></div>
+    <p>Ghassan Albakuaa</p></div>
     <div><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img src="./img/users/dan.jpg" class="users__user_image"></a>
     <p>Dan Rasmussen</p></div>
     <div><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
+    <p>Erin Jodlowsky</p></div>
+    </div>
+    <div><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
     <p>Eric Drexler</p></div>
+    <div><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
+    <p>Nabil</p></div>
+    </div>
     </div>
     `
 }
