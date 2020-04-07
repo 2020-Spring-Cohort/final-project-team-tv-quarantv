da665959ec0bac8489b8a30373652ac0121762b9
Eric Drexler <ericdrxlr@gmail.com>
2020-04-07 13:31:03 -0400

    names added

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -2,10 +2,14 @@ export default function AboutUs() {
     return `
     <p id="about-us">This is the About Us page</p>
     <div id="allpics">
-    <a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"></a>
-    <a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img src="./img/users/ghassan.jpg" class="users__user_image"></a>
-    <a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img src="./img/users/dan.jpg" class="users__user_image"></a>
-    <a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
+    <div><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"></a>
+    <p>Tatyana</p></div>
+    <div><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img src="./img/users/ghassan.jpg" class="users__user_image"></a>
+    <p>Ghassan</p></div>
+    <div><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img src="./img/users/dan.jpg" class="users__user_image"></a>
+    <p>Dan Rasmussen</p></div>
+    <div><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img src="./img/users/eric.jpg" class="users__user_image"></a>
+    <p>Eric Drexler</p></div>
     </div>
     `
-} 
+}
