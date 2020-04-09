15a1906cf1d2d287e3a9b95bb7567befba366cb0
Tatyana Bailey <btatyana45@gmail.com>
2020-04-09 16:45:57 -0400

    one working image and a really bad day

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -1,19 +1,34 @@
 export default function AboutUs() {
     return `
-    <p id="about-us">Meet the team that brought you the QuaranTV aplication</p>
-    <div id="allpics">
-    <div><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image" src="./img/users/tatyana.jpg"><p>Tatyana Bailey</p></a>
-    </div>
-    <div><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img class="users__user_image" src="./img/users/ghassan.jpg"><p>Ghassan Albakuaa</p></a>
-    </div>
-    <div><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img class="users__user_image" src="./img/users/dan.jpg"><p>Dan Rasmussen</p></a>
-    </div>
-    <div><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img class="users__user_image" src="./img/users/eric.jpg"><p>Eric Drexler</p></a>
-    </div>
-    <div><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img class="users__user_image" src="./img/users/erin.jpg"><p>Erin Jodlowsky</p></a>
-    </div>
-    <div><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img class="users__user_image" src="./img/users/nabil.jpg"><p>Nabil Meskaouni</p></a>
-    </div>
-    </div>
+    <p class="about-us">Meet the team that brought you the QuaranTV aplication</p>
+    
+    <section class="users_img_container"
+        <div class="users_imgs">
+
+            <div class="container_user-img"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image user_image" src="./img/users/tatyana.jpg"></a>
+                <div class="img-hover-block">Tatyana Bailey</div>
+            </div>
+
+            <div class="container_user-img><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img  class="users__user_image user_image" src="./img/users/ghassan.jpg"></a>
+                <div class="img-hover-block">Ghassan Albakuaa</div>
+            </div>
+
+            <div class="container_user-img><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img  class="users__user_image user_image" src="./img/users/dan.jpg"></a>
+                <div class="img-hover-block">Dan Rasmussen</div>
+            </div>
+
+            <div class="container_user-img><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img  class="users__user_image user_image" src="./img/users/eric.jpg"></a>
+                <div class="img-hover-block">Eric Drexler</div>
+            </div>
+
+            <div class="container_user-img><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img  class="users__user_image user_image" src="./img/users/erin.jpg"></a>
+                <div class="img-hover-block">Erin Jodlowsky</div>
+            </div>
+
+            <div class="container_user-img><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img  class="users__user_image user_image" src="./img/users/nabil.jpg"></a>
+                <div class="img-hover-block">Nabil Meskaouni</div>
+            </div>
+        </div>
+    </section>
     `
 }
