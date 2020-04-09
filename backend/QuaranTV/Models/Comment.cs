6f182e05c9f813c42aa97fd11579fd78aced8392
anmesk <an_mesk@live.com>
2020-04-09 11:18:29 -0400

    Created Comment model with user ID foreign key

---
+++ backend/QuaranTV/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace QuaranTV.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(int id, string commentText)
+        {
+            Id = id;
+            CommentText = commentText;
+        }
+    }
+}
