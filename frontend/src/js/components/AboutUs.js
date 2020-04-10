3c0cb6bb5fd517aecd8caf995ea6a8f4cdf4c5ce
Tatyana Bailey <btatyana45@gmail.com>
2020-04-09 23:29:49 -0400

    indentation, changed border on img

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -4,32 +4,32 @@ export default function AboutUs() {
     <p class="p__about-us">Reach out and get to know us by clicking bellow</p>
     
     
-        <div class="users_imgs">
+    <div class="users_imgs">
 
-            <div class="flexImg"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="user_img" src="./img/users/tatyana.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank">Tatyana Bailey</a></div>
-            </div>
-
-            <div class="flexImg"><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img  class="user_img" src="./img/users/ghassan.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank">Ghassan Albakuaa</a></div>
-            </div>
-
-            <div class="flexImg"><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img  class="user_img" src="./img/users/dan.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank">Dan Rasmussen</a></div>
-            </div>
-
-            <div class="flexImg"><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img  class="user_img" src="./img/users/eric.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank">Eric Drexler</a></div>
-            </div>
-
-            <div class="flexImg"><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img  class="user_img" src="./img/users/erin.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank">Erin Jodlowsky</a></div>
-            </div>
-
-            <div class="flexImg"><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img  class="user_img" src="./img/users/nabil.jpg"></a>
-                <div class="img-hover-block"><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank">Nabil Meskaouni</a></div>
-            </div>
+        <div class="flexImg"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="user_img" src="./img/users/tatyana.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank">Tatyana Bailey</a></div>
         </div>
+
+        <div class="flexImg"><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img  class="user_img" src="./img/users/ghassan.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank">Ghassan Albakuaa</a></div>
+        </div>
+
+        <div class="flexImg"><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img  class="user_img" src="./img/users/dan.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank">Dan Rasmussen</a></div>
+        </div>
+
+        <div class="flexImg"><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img  class="user_img" src="./img/users/eric.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank">Eric Drexler</a></div>
+        </div>
+
+        <div class="flexImg"><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img  class="user_img" src="./img/users/erin.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank">Erin Jodlowsky</a></div>
+        </div>
+
+        <div class="flexImg"><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img  class="user_img" src="./img/users/nabil.jpg"></a>
+            <div class="img-hover-block"><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank">Nabil Meskaouni</a></div>
+        </div>
+    </div>
    
     `
 }
