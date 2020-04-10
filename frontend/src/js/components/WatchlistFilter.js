2a14c88af6727ebd1640b8748fc74628669eef51
ghassan <gh09an@gmail.com>
2020-04-10 14:02:41 -0400

    delete button for Watch List done

--- frontend/src/js/components/WatchlistFilter.js
+++ frontend/src/js/components/WatchlistFilter.js
@@ -6,7 +6,8 @@ export default function WatchlistFilter(watchlist){
             watchedShow.classList.add("watchlist__watched_show");
             watchedShow.innerText = element.tvShow.title;
             document.querySelector(".watchlist__watched_shows").appendChild(watchedShow);
-            watchedShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">` +
+            watchedShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">
+            <input class="watch__Userid"  type="hidden" value="${element.user.id}">` +
             ` <button class='edit-watchList__submit'>Edit This Show </button>` 
           + ` <button class='addReview-watchList__submit'>Add Review </button>`
           + ` <button class='deleteReview-watchList__submit'>Delete This Show </button>` ;
@@ -17,7 +18,8 @@ export default function WatchlistFilter(watchlist){
             watchingShow.classList.add("watchlist__watching_show");
             watchingShow.innerText = element.tvShow.title;
             document.querySelector(".watchlist__watching_shows").appendChild(watchingShow);
-            watchingShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">` +
+            watchingShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">
+            <input class="watch__Userid"  type="hidden" value="${element.user.id}">` +
             " <button class='edit-watchList__submit'>Edit This Show </button>" 
             + ` <button class='deleteReview-watchList__submit'>Delete This Show </button>` ;
           ;
@@ -27,7 +29,8 @@ export default function WatchlistFilter(watchlist){
             toBeWatchedShow.classList.add("watchlist__tobewatched_show");
             toBeWatchedShow.innerText = element.tvShow.title;
             document.querySelector(".watchlist__tobewatched_shows").appendChild(toBeWatchedShow);
-            toBeWatchedShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">` +
+            toBeWatchedShow.innerHTML +=  `<input class="watch__id"  type="hidden" value="${element.id}">
+            <input class="watch__Userid"  type="hidden" value="${element.user.id}">` +
             " <button class='edit-watchList__submit'>Edit This Show </button>" 
             + " <button class='deleteReview-watchList__submit'>Delete This Show </button>" ;
            ;
