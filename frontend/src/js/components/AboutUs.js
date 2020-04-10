151d6346f8650130f42decc7265e34a5279bec32
Tatyana Bailey <btatyana45@gmail.com>
2020-04-09 22:00:28 -0400

    finished organizing imgs in flexbox with a working name on img hover affect, resized imgs.

--- frontend/src/js/components/AboutUs.js
+++ frontend/src/js/components/AboutUs.js
@@ -2,33 +2,33 @@ export default function AboutUs() {
     return `
     <p class="about-us">Meet the team that brought you the QuaranTV aplication</p>
     
-    <section class="users_img_container"
+    
         <div class="users_imgs">
 
-            <div class="container_user-img"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="users__user_image user_image" src="./img/users/tatyana.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/tatyanaasb/" target="_blank"><img class="user_img" src="./img/users/tatyana.jpg"></a>
                 <div class="img-hover-block">Tatyana Bailey</div>
             </div>
 
-            <div class="container_user-img><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img  class="users__user_image user_image" src="./img/users/ghassan.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/ghassan-albakuaa-84b81767/" target="_blank"><img  class="user_img" src="./img/users/ghassan.jpg"></a>
                 <div class="img-hover-block">Ghassan Albakuaa</div>
             </div>
 
-            <div class="container_user-img><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img  class="users__user_image user_image" src="./img/users/dan.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/dan-rasmussen-04b4811a2/" target="_blank"><img  class="user_img" src="./img/users/dan.jpg"></a>
                 <div class="img-hover-block">Dan Rasmussen</div>
             </div>
 
-            <div class="container_user-img><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img  class="users__user_image user_image" src="./img/users/eric.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/eric-drexler/" target="_blank"><img  class="user_img" src="./img/users/eric.jpg"></a>
                 <div class="img-hover-block">Eric Drexler</div>
             </div>
 
-            <div class="container_user-img><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img  class="users__user_image user_image" src="./img/users/erin.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/erinjodlowsky/" target="_blank"><img  class="user_img" src="./img/users/erin.jpg"></a>
                 <div class="img-hover-block">Erin Jodlowsky</div>
             </div>
 
-            <div class="container_user-img><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img  class="users__user_image user_image" src="./img/users/nabil.jpg"></a>
+            <div class="flexImg"><a href="https://www.linkedin.com/in/nabil-meskaouni-b63818181/" target="_blank"><img  class="user_img" src="./img/users/nabil.jpg"></a>
                 <div class="img-hover-block">Nabil Meskaouni</div>
             </div>
         </div>
-    </section>
+   
     `
 }
