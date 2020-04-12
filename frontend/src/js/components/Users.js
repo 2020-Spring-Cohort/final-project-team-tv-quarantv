5974b8d56e7ae089082f5696efd68a7bece9ab1b
ghassan <gh09an@gmail.com>
2020-04-12 19:15:37 -0400

    edited users Component

--- frontend/src/js/components/Users.js
+++ frontend/src/js/components/Users.js
@@ -1,14 +1,14 @@
 export default function Users(users){
     //const Login_User__Id = event.target.parentElement.querySelector('.add-__username').value;
-    
-alert("user id =" + Login_User__Id);
+
+//alert("user id =" + Login_User__Id);
     return `
     <h2>All Users</h2>
         <div class="users__grid_container">
             ${users.map(user => {
-                localStorage.clear();
-                localStorage.setItem("vistorId",user.id);               
-                localStorage.setItem("vistorName",user.name);
+      //          localStorage.clear();
+       //         localStorage.setItem("vistorId",user.id);               
+       //         localStorage.setItem("vistorName",user.name);
                 return `
                 <div class="users__specific_user"
                     <h4 class="user__name_btn">${user.name}</h4>
