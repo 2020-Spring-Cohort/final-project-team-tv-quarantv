fc5d73bb48cb502bcdfcd11d2bf8b45015135667
anmesk <an_mesk@live.com>
2020-04-13 14:59:44 -0400

    Added if statement to TvShowSelection to keep null reviews from displaying

--- frontend/src/js/components/TvShowSelection.js
+++ frontend/src/js/components/TvShowSelection.js
@@ -11,16 +11,19 @@ export default function TvShowSelection(tvShow) {
             </div>
             <div class="tvShowSelection__text_area">
                 ${tvShow.watchlists.map(value => {
-                    return `
-                    <div class="tvShowSelection__specific_user"
-                        <p></p>
-                        <h4 class="users__name_btn">Name: ${value.user.name}</h4>
-                        <h4 class="users__show_status">Status: ${value.status}</h4>
-                        <h4 class="users__show__rating">Rating: ${value.rating}</h4>
-                        <h4 class="users__show_review">Review: ${value.review}</h4>
-                        <input class="users__id" type="hidden" value="${value.userId}">
-                    </div>
-                    `
+                    if(value.review != null)
+                    {
+                        return `
+                        <div class="tvShowSelection__specific_user"
+                            <p></p>
+                            <h4 class="users__name_btn">Name: ${value.user.name}</h4>
+                            <h4 class="users__show_status">Status: ${value.status}</h4>
+                            <h4 class="users__show__rating">Rating: ${value.rating}</h4>
+                            <h4 class="users__show_review">Review: ${value.review}</h4>
+                            <input class="users__id" type="hidden" value="${value.userId}">
+                        </div>
+                        `
+                    }
                 }).join("")}
             </div>
 
