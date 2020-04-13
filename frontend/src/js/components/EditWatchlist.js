4e49db3fe2bdb7fa90d8035869302511aba9f048
anmesk <an_mesk@live.com>
2020-04-13 19:07:32 -0400

    Replaced Rating text box in Edit Show page with dropdown menu

--- frontend/src/js/components/EditWatchlist.js
+++ frontend/src/js/components/EditWatchlist.js
@@ -17,14 +17,20 @@ export default function EditWatchlist(watch){
      <label >Review</label><br>
      <input class="update-review" type="text" value="${watch.review}"><br><br>
      <label >Rating</label><br>
-     <input class="update-rating" type="text" value="${watch.rating}"><br><br> 
+     <select class="update-rating" type="dropdown">
+     <option value="1">1</option>
+     <option value="2">2</option>
+     <option value="3">3</option>     
+     <option value="4">4</option>
+     <option value="5">5</option>   
+     </select><br><br>
+
      <input class="watch__id" type="hidden" value="${watch.id}">
      <input class="user__id" type="hidden" value="${watch.userId}">
      <input class="tv__id" type="hidden" value="${watch.tvShowId}">
         
      <button class="update-watchList__submit">Save Changes</button>
      </section>
- 
  `;
  
  if(watch.status == "Watching")
