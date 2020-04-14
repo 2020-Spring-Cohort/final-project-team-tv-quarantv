c1feb4ebff1f635a586431e2ec76f88e588fc17c
ghassan <gh09an@gmail.com>
2020-04-14 08:54:38 -0400

    fix add tv show dropdown in watchlist and add stars and watched watching to tvshow

--- frontend/src/js/components/TvShows.js
+++ frontend/src/js/components/TvShows.js
@@ -1,13 +1,155 @@
 export default function TvShows(tvShows){
+    var  WatchedSum =0;
+    var WatchingSum =0;
+    var RatingSum =0;
+    var Watched =[];
+    var ToBeWatched =[];
+    var Watching =[];
+    var Count = 0; 
+    var Stars=0;
     return `
     <section class="tvShows">
         <h2>All TV Shows</h2>
         <div class="tvShows__grid_container">
-            ${tvShows.map(tvShow => {
-                return `
+            ${tvShows.map(tvShow => {   
+                WatchedSum=0;  
+                WatchingSum=0; 
+                Stars=0;  
+                Count = 0;  
+                WatchingSum =0;
+                RatingSum=0;
+
+                  tvShow.watchlists.forEach(element => {
+                    if(tvShow.id == element.tvShowId)
+                    {
+                        if(element.status == "Watched")
+                        {
+                            Watched.push(element.user.name , tvShow.title ,element.rating);
+                            WatchedSum ++;
+                            if(element.rating != null)
+                            {
+                              RatingSum +=  eval (element.rating);                              
+                              Count++;
+                            //  alert("rateing ="+ element.rating)
+                            }  
+                          
+                        }
+                       
+
+                        if(element.status == "ToBeWatched")
+                        {
+                            ToBeWatched.push(element.user.name , tvShow.title );
+                            //ToBeWatchedSum ++;
+                        }
+                       
+
+                        if(element.status == "Watching")
+                        {
+                            Watching.push(element.user.name , tvShow.title );
+                            WatchingSum ++;
+                        }
+
+                           
+                    }   
+                           
+                 });     
+                 Stars = (RatingSum / Count);
+                 if(Stars >= 1)
+                 {
+                    if(Stars >= 1 && Stars <2)
+                    return `             
+                   
+                    <div class="tvShows__specific_tvShow">
+                        <artice>
+                            <h4 class="tvShows__show_title">${tvShow.title} 
+                            <h3>Watched = ${WatchedSum}</h3><br>
+                            <h3>Watching = ${WatchingSum}</h3><br>  
+                            <h3>Rating = ${Stars}<br><img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                            <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                            <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                            <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                            </h3><br>
+                            <p class="p_title-hover-btn">>></p></h4>
+                            <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
+                            <input class="tvShow__id" type="hidden" value="${tvShow.id}">
+                        <artice>
+                        <div class ="dropdown-content">
+                            <p class="p-dropdown">Start Watching</p>
+                            <a href="https://www.netflix.com/" target="_blank"><img class="icon" src="./img/design/netflix.jpg" alt="netflix.com"></a>
+                            <a href="https://www.amazon.com/" target="_blank"><img class="icon" src="./img/design/amazon.jpg" alt="amazon.com"></a>
+                            </br>
+                            <a href="https://www.imdb.com/" target="_blank"><img class="icon" src="./img/design/imdb.jpg" alt="imdb.com"></a>
+                            <a href="https://www.youtube.com/" target="_blank"><img class="icon" src="./img/design/youtube.jpg" alt="youtube.com"></a>
+                        </div>
+                    </div>
+                   `
+                   if(Stars >= 2 && Stars <3)
+                   return `             
+                  
+                   <div class="tvShows__specific_tvShow">
+                       <artice>
+                           <h4 class="tvShows__show_title">${tvShow.title} 
+                           <h3>Watched = ${WatchedSum}</h3><br>
+                           <h3>Watching = ${WatchingSum}</h3><br>  
+                           <h3>Rating = ${Stars}<br><img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                           <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                        
+                           </h3><br>
+                           <p class="p_title-hover-btn">>></p></h4>
+                           <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
+                           <input class="tvShow__id" type="hidden" value="${tvShow.id}">
+                       <artice>
+                       <div class ="dropdown-content">
+                           <p class="p-dropdown">Start Watching</p>
+                           <a href="https://www.netflix.com/" target="_blank"><img class="icon" src="./img/design/netflix.jpg" alt="netflix.com"></a>
+                           <a href="https://www.amazon.com/" target="_blank"><img class="icon" src="./img/design/amazon.jpg" alt="amazon.com"></a>
+                           </br>
+                           <a href="https://www.imdb.com/" target="_blank"><img class="icon" src="./img/design/imdb.jpg" alt="imdb.com"></a>
+                           <a href="https://www.youtube.com/" target="_blank"><img class="icon" src="./img/design/youtube.jpg" alt="youtube.com"></a>
+                       </div>
+                   </div>
+                  `
+                  if(Stars >= 3 && Stars <4)
+                  return `             
+                 
+                  <div class="tvShows__specific_tvShow">
+                      <artice>
+                          <h4 class="tvShows__show_title">${tvShow.title} 
+                          <h3>Watched = ${WatchedSum}</h3><br>
+                          <h3>Watching = ${WatchingSum}</h3><br>  
+                          <h3>Rating = ${Stars}<br><img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                          <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                          <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                         
+                          </h3><br>
+                          <p class="p_title-hover-btn">>></p></h4>
+                          <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
+                          <input class="tvShow__id" type="hidden" value="${tvShow.id}">
+                      <artice>
+                      <div class ="dropdown-content">
+                          <p class="p-dropdown">Start Watching</p>
+                          <a href="https://www.netflix.com/" target="_blank"><img class="icon" src="./img/design/netflix.jpg" alt="netflix.com"></a>
+                          <a href="https://www.amazon.com/" target="_blank"><img class="icon" src="./img/design/amazon.jpg" alt="amazon.com"></a>
+                          </br>
+                          <a href="https://www.imdb.com/" target="_blank"><img class="icon" src="./img/design/imdb.jpg" alt="imdb.com"></a>
+                          <a href="https://www.youtube.com/" target="_blank"><img class="icon" src="./img/design/youtube.jpg" alt="youtube.com"></a>
+                      </div>
+                  </div>
+                 `
+                 if(Stars >= 4 && Stars <5)
+                return `             
+               
                 <div class="tvShows__specific_tvShow">
                     <artice>
-                        <h4 class="tvShows__show_title">${tvShow.title} <p class="p_title-hover-btn">>></p></h4>
+                        <h4 class="tvShows__show_title">${tvShow.title} 
+                        <h3>Watched = ${WatchedSum}</h3><br>
+                        <h3>Watching = ${WatchingSum}</h3><br>  
+                        <h3>Rating = ${Stars}<br><img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                        <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                        </h3><br>
+                        <p class="p_title-hover-btn">>></p></h4>
                         <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
                         <input class="tvShow__id" type="hidden" value="${tvShow.id}">
                     <artice>
@@ -21,8 +163,81 @@ export default function TvShows(tvShows){
                     </div>
                 </div>
                `
+               if(Stars >= 5)
+               return `             
+              
+               <div class="tvShows__specific_tvShow">
+                   <artice>
+                       <h4 class="tvShows__show_title">${tvShow.title} 
+                       <h3>Watched = ${WatchedSum}</h3><br>
+                       <h3>Watching = ${WatchingSum}</h3><br>  
+                       <h3>Rating = ${Stars}<br><img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       <img width="25px" src="https://clipartmag.com/images/stars-transparent-background-21.gif"/>
+                       </h3><br>
+                       <p class="p_title-hover-btn">>></p></h4>
+                       <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
+                       <input class="tvShow__id" type="hidden" value="${tvShow.id}">
+                   <artice>
+                   <div class ="dropdown-content">
+                       <p class="p-dropdown">Start Watching</p>
+                       <a href="https://www.netflix.com/" target="_blank"><img class="icon" src="./img/design/netflix.jpg" alt="netflix.com"></a>
+                       <a href="https://www.amazon.com/" target="_blank"><img class="icon" src="./img/design/amazon.jpg" alt="amazon.com"></a>
+                       </br>
+                       <a href="https://www.imdb.com/" target="_blank"><img class="icon" src="./img/design/imdb.jpg" alt="imdb.com"></a>
+                       <a href="https://www.youtube.com/" target="_blank"><img class="icon" src="./img/design/youtube.jpg" alt="youtube.com"></a>
+                   </div>
+               </div>
+              `
+                 }
+               else 
+             
+              
+               return `             
+              
+               <div class="tvShows__specific_tvShow">
+                   <artice>
+                       <h4 class="tvShows__show_title">${tvShow.title} 
+                       <h3>Watched = ${WatchedSum}</h3><br>
+                       <h3>Watching = ${WatchingSum}</h3><br> 
+                       <p class="p_title-hover-btn">>></p></h4>
+                       <img class="tvShows__show_image" src="./img/tvshows/${tvShow.image}" alt="TvShow Image">
+                       <input class="tvShow__id" type="hidden" value="${tvShow.id}">
+                   <artice>
+                   <div class ="dropdown-content">
+                       <p class="p-dropdown">Start Watching</p>
+                       <a href="https://www.netflix.com/" target="_blank"><img class="icon" src="./img/design/netflix.jpg" alt="netflix.com"></a>
+                       <a href="https://www.amazon.com/" target="_blank"><img class="icon" src="./img/design/amazon.jpg" alt="amazon.com"></a>
+                       </br>
+                       <a href="https://www.imdb.com/" target="_blank"><img class="icon" src="./img/design/imdb.jpg" alt="imdb.com"></a>
+                       <a href="https://www.youtube.com/" target="_blank"><img class="icon" src="./img/design/youtube.jpg" alt="youtube.com"></a>
+                   </div>
+               </div>
+              `
             }).join("")}
+
+
+            
         </div>
     </section>
     `
 }
+/*
+ <div class="tvShowSelection__text_area">
+                ${tvShow.watchlists.map(value => {
+                    return `
+                    <div class="tvShowSelection__specific_user"
+                        <p></p>
+                        <h4 class="users__name_btn">Name: ${value.user.name}</h4>
+                        <h4 class="users__show_status">Status: ${value.status}</h4>
+                        <h4 class="users__show__rating">Rating: ${value.rating}</h4>
+                        <h4 class="users__show_review">Review: ${value.review}</h4>
+                        <input class="users__id" type="hidden" value="${value.userId}">
+                    </div>
+                    `
+                }).join("")}
+            </div>
+
+*/
--- frontend/src/js/components/WatchlistAddShow.js
+++ frontend/src/js/components/WatchlistAddShow.js
@@ -1,11 +1,20 @@
 //import UserLogin from "./UserLogin"
-
+var flag = 0;
 export default function WatchlistAddShow(tvShows) {
  
     return `
     <p>Choose a show</p>
     <select class="watchlistaddshow__show_id" type="dropdown">
     ${tvShows.map(tvShow => {
+        flag = 0;
+        tvShow.watchlists.forEach(element => {
+         if(tvShow.id == element.tvShowId && element.userId == localStorage.LoginhUserId)
+         {
+           flag =1;
+         }
+        })
+          
+       if(flag == 0)
         return `
             <option value=${tvShow.id}>${tvShow.title}</option>
             `
@@ -22,3 +31,37 @@ export default function WatchlistAddShow(tvShows) {
     `
     
 }
+
+
+/*
+   return `
+    <p>Choose a show</p>
+    <select class="watchlistaddshow__show_id" type="dropdown">
+    ${tvShows.map(tvShow => {
+        flag = 0;
+        tvShow.watchlists.forEach(element => {
+         if(tvShow.id == 1)
+         {
+           flag =1;
+         }
+        })
+          
+       if(flag == 0)
+        return `
+            <option value=${tvShow.id}>${tvShow.title}</option>
+            `
+        })}
+    </select>
+    <p>Choose a status</p>
+    <select class="watchlistaddshow__status_choice" type="dropdown">
+        <option value="ToBeWatched">To Be Watched</option>
+        <option value="Watching">Watching</option>
+        <option value="Watched">Watched</option>
+    </select>
+    <br><br>
+    <button class="watchlistaddshow__submit">Add show to watchlist</button>
+    `
+    
+}
+
+*/
