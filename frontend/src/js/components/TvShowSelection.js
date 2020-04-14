38d364e576a2a7e14dbbf465b7acf1655b1f6361
Tatyana Bailey <btatyana45@gmail.com>
2020-04-14 15:18:25 -0400

    tvshow selection info of spesific show is styled, started adding background to views, changed spacings

--- frontend/src/js/components/TvShowSelection.js
+++ frontend/src/js/components/TvShowSelection.js
@@ -1,32 +1,26 @@
 export default function TvShowSelection(tvShow) {
     return `
         <div class="tvShow__grid_container">
-            <div class="tvShowSelection__tvShow_info"
-                <h2>${tvShow.title}</h2>
-                <img class="tvShow__image" src="./img/tvshows/${tvShow.image}" alt="${tvShow.image}"></img>
-                <h4>Number of seasons: ${tvShow.season}</h4>
-                <h4>Rated: ${tvShow.viewerDiscretion}</h4>
+        <div class="tvShowSelection__tvShow_info">
+        <h2>${tvShow.title}</h2>
+        <h4># of Seasons: ${tvShow.season} - Rated: ${tvShow.viewerDiscretion}</h4>
+        <img class="tvShow__images" src="./img/tvshows/${tvShow.image}" alt="${tvShow.image}"></img>
                 <input class="tvShow__id" type="hidden" value="${tvShow.id}">
                 </br>
             </div>
             <div class="tvShowSelection__text_area">
+            <h3>Popular Reviews</h3>
+            <hr>
                 ${tvShow.watchlists.map(value => {
-                    if(value.review != null)
-                    {
-                        if (value.review != "")
-                        {
-                            return `
-                            <div class="tvShowSelection__specific_user"
-                                <p></p>
-                                <h4 class="users__name_btn">Name: ${value.user.name}</h4>
-                                <h4 class="users__show_status">Status: ${value.status}</h4>
-                                <h4 class="users__show__rating">Rating: ${value.rating}</h4>
-                                <h4 class="users__show_review">Review: ${value.review}</h4>
-                                <input class="users__id" type="hidden" value="${value.userId}">
-                            </div>
-                            `
-                        }
-                    }
+                    return `
+                    <div class="tvShowSelection__specific_user">
+                        <h4 class="users__name_btn">Review by <span style="color:blue;">${value.user.name}</span></h4>
+                        <h4 class="users__show_status">Status: ${value.status } - Rating: ${value.rating}</h4>
+                        <h4 class="users__show_review"><i>Review: ${value.review}</i></h4>
+                        <input class="users__id" type="hidden" value="${value.userId}">
+                    </div>
+                    <hr>
+                    `
                 }).join("")}
             </div>
 
