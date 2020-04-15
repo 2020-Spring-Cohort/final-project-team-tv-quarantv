f1b0a1e6f0d21465f2921610e36081ce04646f40
Tatyana Bailey <btatyana45@gmail.com>
2020-04-15 11:51:11 -0400

    Add a comment section is styled, refactored small things

--- frontend/src/js/components/CommentsByTvShow.js
+++ frontend/src/js/components/CommentsByTvShow.js
@@ -1,26 +1,25 @@
 export default function CommentsByTvShow(comments){
     return `
     <h2>All Comments</h2>
-        <div class="comments__grid_container">
-            ${comments.map(comments => {
-                return `
-                <ul class="comments">
+    <!--- Add Comments here --->
+    <section class="add-comment">
+        </br>
+        <button class="add-comment__submit">Post a Comment</button>       
+        </br>
+        <textarea class="add-commentText" type="text" rows="5" cols="33" placeholder="Add a Comment"></textarea>
+    </section>
+    <div class="comments__grid_container">
+        ${comments.map(comments => {
+            return `
+            <ul class="comments">
                 <li>
-                <p>${comments.user.name} writes:<p>
-                <p>${comments.commentText}</p>
+                    <p>${comments.user.name} writes:<p>
+                    <p>${comments.commentText}</p>
                 </li>
-                </ul>
-               `
-        }).join("")}
-        </div>
+            </ul>
+            `
+    }).join("")}
+    </div>
 
-        <!--- Add Comments here --->
-        <section class="add-comment">
-            <label>Post a Comment</label>
-            </br>
-            <textarea class="add-commentText" type="text" rows="5" cols="33" placeholder="Add a Comment"></textarea>
-            </br>
-            <button class="add-comment__submit">Add a Comment</button>       
-        </section>
     `
 }
