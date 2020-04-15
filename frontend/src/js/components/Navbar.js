1c9b4edf92fc9d08d71edd6c14f98fb323050a06
ghassan <gh09an@gmail.com>
2020-04-15 11:18:51 -0400

    the header script and css done

--- frontend/src/js/components/Navbar.js
+++ frontend/src/js/components/Navbar.js
@@ -1,5 +1,20 @@
 export default function Navbar() {
-return `
+
+   return `
+   <table class='navbar_list'  id='user'>
+            <tr class='navbarlist'>
+               <td class='nav__home side-nav__item'>Log In</td>
+               <td class='nav__users side-nav__item'>Members</td>
+               <td class='nav__tvshows side-nav__item'>Tv Shows</td>
+               <td class='nav__aboutus side-nav__item'>About Us</td>
+            </tr>
+       </table>
+   
+       ` 
+}
+/*
+
+   return `
 <nav class='navbar_list'>
          <div class='navbarlist'>
             <div class='nav__home'>Home</div>
@@ -10,3 +25,4 @@ return `
     </nav>
     ` 
 }
+*/
