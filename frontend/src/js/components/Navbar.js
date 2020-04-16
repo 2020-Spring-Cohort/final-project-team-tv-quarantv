85614c1b1d4b0df41f71fe0af34b4e1ac5df950e
Dan Rasmussen <drasmussen49@gmail.com>
2020-04-15 20:11:26 -0400

    Styling changes to many components, mostly header and footer

--- frontend/src/js/components/Navbar.js
+++ frontend/src/js/components/Navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
 return `
    <nav class='navbar_list'>
          <ul class='navbarlist'>
-            <li class='nav__home'>Home/Login</li>
+            <li class='nav__home'>Home Login</li>
             <li class='nav__users'>Members</li>
             <li class='nav__tvshows'>Tv Shows</li>
             <li class='nav__aboutus'>About Us</li>
